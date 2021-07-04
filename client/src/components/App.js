134640f55ca1f6f9755c8249090461e02b578b85
IT19071480 <IT19071480@my.sliit.lk>
2021-07-04 21:38:42 +0530

    NavBar Setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
-
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './Header';
 
 const App = () => (
-  <div className="App">
-    <p>Inside App Component</p>
-  </div>
+  <BrowserRouter>
+    <Header />  
+  </BrowserRouter>
+
 );
+
 
 
 export default App;
