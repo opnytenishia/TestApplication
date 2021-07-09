6abaf90a50008e162e6a88ac1051a4e5219d1389
IT19071480 <IT19071480@my.sliit.lk>
2021-07-09 16:28:18 +0530

    NavBar Update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,30 +6,32 @@ const Header = () => {
     //views
 
     const showNavigation = () => (
+
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <Link to = "#" className="navbar-brand">
+                <Link to="#" className="navbar-brand" >
                     Logo
                 </Link>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-                    <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link to = "#" className="nav-link" aria-current="page">
-                                SignUp
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="#" className="nav-link">
-                                SignIn
-                            </Link>
-                        </li>
-                    </ul>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto mb-2 mb-lg-0 ">
+                    <li className="nav-item">
+                        <Link to="#" className="nav-link" aria-current="page" >
+                            SignUp
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="#" className="nav-link" >
+                            SignIn
+                        </Link>
+                    </li>
+                </ul>
                 </div>
             </div>
         </nav>
+
     );
 
 
